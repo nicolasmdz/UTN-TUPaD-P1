53820044a70a5efd89ba2435f26061c1cffdd890
nicolasmdz <nicolasmonacoojeda@gmail.com>
2025-04-19 20:08:01 -0300

    renombro la carpeta del TP

---
+++ 04 Estructuras Repetitivas/nicoalasMonacoTP-05.py
@@ -0,0 +1,10 @@
+num=int(input("Ingrese un numero positivo: "))
+if num > 0:
+    if num % 2 != 0:
+        num - 1
+    cont= numero
+    while cont >= 0:
+        print(str(cont)+ " ", end="")
+        cont=cont -2
+    else:
+        print("el numero debia ser positivo")
